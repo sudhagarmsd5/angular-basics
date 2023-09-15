747dd1d04afdb9ff175d7e0d86bd14aaa6fe5309
sudhagarmsd5 <sudhagarmsd@gmail.com>
2023-09-15 17:40:10 +0530

    code changes configured default routing with hasing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DirectiveExampleComponent } from './directive-example/directive-example
 import { PipeExampleComponent } from './pipe-example/pipe-example.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'',pathMatch:'full'},
+  {path:'',redirectTo:'component',pathMatch:'full'},
   {path:'component',component:ComponentExampleComponent},
   {path:'directive',component:DirectiveExampleComponent},
   {path:'pipe',component:PipeExampleComponent},
@@ -14,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
